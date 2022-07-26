8750410da46f7144acce74b13a57c85a7eca8f43
PuffinDev <puffin.develop@gmail.com>
2022-07-26 10:59:12 +0100

    Create simple 3D ISS display

---
+++ iss_display/iss_display.py
@@ -0,0 +1,39 @@
+from ursina import Ursina, Entity, EditorCamera
+from utils import latlon_to_coords
+from threading import Thread
+from time import sleep
+import requests
+import json
+
+app = Ursina()
+
+def display_point(lat, lon, alt):
+    return Entity(model="sphere", scale=0.0025, position=latlon_to_coords(lat, lon, alt))
+
+earth = Entity(model="sphere", texture="earth8k.jpg")
+
+iss = display_point(0, 0, 100)
+pos = {"latitude": 0, "longitude": 0, "altitude": 0}
+
+def get_pos():
+    global pos
+
+    while True:
+        result = requests.get("https://api.wheretheiss.at/v1/satellites/25544")
+        if result.status_code != 200:
+            return
+        
+        pos = result.json()
+        print(pos)
+        sleep(1)
+
+def update():
+    global pos
+    iss.position = latlon_to_coords(pos["latitude"], pos["longitude"], pos["altitude"])
+
+EditorCamera()
+
+get_pos_thread = Thread(target=get_pos)
+get_pos_thread.start()
+
+app.run()
---
+++ iss_display/utils.py
@@ -0,0 +1,19 @@
+from math import radians, tan, atan, cos, sin
+from ursina import Vec3
+
+KM = 1/12742
+RAD = 0.5
+
+def latlon_to_coords(lat, lon, alt):
+    lat = radians(lat)
+    lon = radians(lon)
+    alt = alt*KM
+
+    f  = 0
+    ls = atan((1 - f)**2 * tan(lat))
+
+    x = RAD * cos(ls) * cos(lon) + alt * cos(lat) * cos(lon)
+    y = RAD * cos(ls) * sin(lon) + alt * cos(lat) * sin(lon)
+    z = RAD * sin(ls) + alt * sin(lat)
+
+    return Vec3(x, y, z)
