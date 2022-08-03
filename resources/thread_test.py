8f00011bfecc1741f16763cec5685610628d64e1
John <jwgbradford@gmail.com>
2022-08-03 12:26:49 +0100

    threads not stopping

--- resources/thread_test.py
+++ resources/thread_test.py
@@ -3,26 +3,55 @@
 # threads using stop
 # flag
  
-import threading
+from threading import Thread, Event
 import time
- 
-def run(stop):
-    while True:
-        print('thread running')
-        time.sleep(0.2)
-        if stop():
-                break
 
-def main():
-    while True:
-        stop_threads = False
-        t1 = threading.Thread(target = run, args =(lambda : stop_threads, ))
-        t1.start()
-        print('thread started')
-        time.sleep(1)
-        stop_threads = True
-        t1.join()
-        print('thread killed')
-        time.sleep(1)
+class StoppableThread(Thread):
+    def __init__(self,  *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = Event()
 
-main()
+    def stop(self):
+        self._stop_event.set()
+
+    @property
+    def stopped(self):
+        return self._stop_event.is_set()
+class MyApp():
+    def __init__(self) -> None:
+        pass
+
+    def update(self):
+        while True:
+            print('updating everything')
+            time.sleep(0.1)
+
+    def density_update(self):
+        while True:
+            print('updating densities')
+            time.sleep(0.3)
+
+    def start_threads(self):
+        self.update_thread = StoppableThread(target=self.update)
+        self.update_thread.start()
+        self.density_update_thread = StoppableThread(target=self.density_update)
+        self.density_update_thread.start()
+
+    def restart_threads(self):
+        print('stopping threads')
+        self.update_thread.stop()
+        self.update_thread.join()
+        self.density_update_thread.stop()
+        self.density_update_thread.join()
+        self.print("Changed dataset")
+        self.start_threads()
+        
+    def main(self):
+        while True:
+            self.start_threads()
+            time.sleep(1)
+            self.restart_threads()
+
+if __name__ == "__main__":
+    my_app = MyApp()
+    my_app.main()
